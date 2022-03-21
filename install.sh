9172c519ad2020ab751cfd9e1426980d556c635a
Zarcolio <registered@quicknet.nl>
2022-03-21 11:28:39 +0100

    Ran it through shellcheck.net

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ dir=$(pwd)
 		while true; do
 			read -p -r "2ulb not found, install 2ulb? [y/n]: " yn
 			case $yn in
-			    [Yy]*) cd .. || exit; git clone https://github.com/Zarcolio/2ulb ; sudo python3 2ulb/2ulb.py 2ulb/2ulb.py ; cd $dir || exit; sudo 2ulb $scriptname; exit 0 ;;  
+			    [Yy]*) cd .. || exit; git clone https://github.com/Zarcolio/2ulb ; sudo python3 2ulb/2ulb.py 2ulb/2ulb.py ; cd "$dir" || exit; sudo 2ulb $scriptname; exit 0 ;;  
 			    [Nn]*) echo "Aborted" ; exit 1 ;;
 			esac
 		done
