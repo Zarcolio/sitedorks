fffbe2c731855b0e2fbc5abd21cb090fbd87ffdf
Zarcolio <registered@quicknet.nl>
2022-08-09 11:27:51 -0400

    Removed -r from read -p, now the install script works again

--- install.sh
+++ install.sh
@@ -9,8 +9,9 @@ dir=$(pwd)
 2ulb 2&>/dev/null
 	if [ $? -eq 127 ]
 	then	
+		echo "2ulb not found, install 2ulb? [y/n]: "
 		while true; do
-			read -p -r "2ulb not found, install 2ulb? [y/n]: " yn
+			read yn -p
 			case $yn in
 			    [Yy]*) cd .. || exit; git clone https://github.com/Zarcolio/2ulb ; sudo python3 2ulb/2ulb.py 2ulb/2ulb.py ; cd "$dir" || exit; sudo 2ulb $scriptname; exit 0 ;;  
 			    [Nn]*) echo "Aborted" ; exit 1 ;;
